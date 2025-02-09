edd9d70d792ea765ae05915590a3e70cefb0bae6
joshnin40 <joshnin@gmail.com>
2025-02-08 21:21:10 -0500

    Update streamlit_app.py
    The requests library allows us to build and sent REST API calls.  Paste the code below into the bottom of your SniS app

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,4 +48,6 @@ if time_to_insert:
 
     st.success('Your Smoothie is ordered!', icon="✅")
     
-
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)        
