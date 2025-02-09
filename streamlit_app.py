f8f9fa0baefdc2faf99fe8b08cce49f21e915396
joshnin40 <joshnin@gmail.com>
2025-02-09 14:28:16 -0500

    Update streamlit_app.py
    People often use df as shorthand for "dataframe." We'll call our dataframe sf_df, because it's our SmoothieFroot Dataframe.

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,4 +50,5 @@ if time_to_insert:
     
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.jason())        
+#st.text(smoothiefroot_response.jason())  
+st_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = True)
