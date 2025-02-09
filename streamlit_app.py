6ad5b10644a1ec8622aabaf9a80143dfc3c09787
joshnin40 <joshnin@gmail.com>
2025-02-09 14:22:35 -0500

    Update streamlit_app.py
    Adding response of Jason

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,4 +50,4 @@ if time_to_insert:
     
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)        
+st.text(smoothiefroot_response.jason())        
